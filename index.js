b051b170e73dcdac56b13630c3ce9e1068d405cd
Ronald M. Clifford <roncli@roncli.com>
2018-11-09 17:14:24 -0800

    Update console title.

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ const Discord = require("./discord"),
 (function startup() {
     Log.log("Starting up...");
 
+    if (process.platform === "win32") {
+        process.title = "Overload Teams League";
+    } else {
+        process.stdout.write("\x1b]2;Overload Teams League\x1b\x5c");
+    }
+
     // Startup Discord.
     Discord.startup();
     Discord.connect();
