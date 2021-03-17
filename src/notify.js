81f0b3cbc1835d92cdb66c99bb25a4a6764c6a2c
Ronald M. Clifford <roncli@roncli.com>
2021-03-17 10:26:05 -0700

    Remove dead code.

--- src/notify.js
+++ src/notify.js
@@ -15,21 +15,6 @@ let setup = false;
  * A class that handles Discord notifications.
  */
 class Notify {
-    //              #     #      #
-    //              #           # #
-    // ###    ##   ###   ##     #    #  #
-    // #  #  #  #   #     #    ###   #  #
-    // #  #  #  #   #     #     #     # #
-    // #  #   ##     ##  ###    #      #
-    //                                #
-    /**
-     * Runs all notification functions.
-     * @returns {Promise} A promise that resolves when all notifications are sent.
-     */
-    static notify() {
-        return Challenge.notify();
-    }
-
     //               #                #  #         #     #      #    #                 #     #
     //               #                ## #         #           # #                     #
     //  ###    ##   ###   #  #  ###   ## #   ##   ###   ##     #    ##     ##    ###  ###   ##     ##   ###    ###
